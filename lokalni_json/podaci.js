858bf228579ca668c1738904d06b3cb9575122cb
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2024-01-13 18:47:01 +0100

    pocetak stranice

---
+++ lokalni_json/podaci.js
@@ -0,0 +1,41 @@
+//polje...
+var podaci=[
+    //prvi element = JSON oblik zapisa
+    {
+        "id":"1",
+        "ime":"Josip",
+        "prezime":"Mirković",
+        "slika":"https://as2.ftcdn.net/v2/jpg/03/16/76/07/1000_F_316760793_VwaEcqW6A4D5A6Li9j4ArqY2i0KuSS5o.jpg" ,
+        "email":"josip@mirkovic.hr"    
+    },
+ //drugi element
+    {
+        "id":"2",
+        "ime":"Ivana",
+        "prezime":"Mirković",
+        "slika":"https://as2.ftcdn.net/v2/jpg/03/16/76/07/1000_F_316760793_VwaEcqW6A4D5A6Li9j4ArqY2i0KuSS5o.jpg" ,
+        "email":"ivana@mirkovic.hr"    
+    }
+     //n elementa ..
+];
+
+//polje...
+var narudzbe=[
+    //prvi element = JSON oblik zapisa
+    {
+        "id":"1",
+        "brNar":"100",
+        "partner":"Đuro Đaković spec."
+    }
+     //n elementa ..
+];
+
+var artikli=[
+    //prvi element = JSON oblik zapisa
+    {
+        "id":"1",
+        "brArt":"1",
+        "naziv":"Opruga"
+    }
+     //n elementa ..
+];

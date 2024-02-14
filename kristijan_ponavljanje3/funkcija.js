7516b40ddbf0cace750467452d9503ab5ee1d232
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2024-02-14 20:27:32 +0100

    update

--- kristijan_ponavljanje3/funkcija.js
+++ kristijan_ponavljanje3/funkcija.js
@@ -1,21 +1,45 @@
 function postavi() {
     var radioGumbi = document.getElementsByName("spol");
+    var lista = document.getElementById("odabirSpola");
     var rezultatLabela = document.getElementById("rezultat");
+
+    var odabranaVrijednost = "";
 
     for (var i = 0; i < radioGumbi.length; i++) {
         if (radioGumbi[i].checked) {
-            rezultatLabela.innerHTML = "Odabrani spol: " + radioGumbi[i].value;
+            odabranaVrijednost = radioGumbi[i].value;
+            break;
+        }
+    }
+
+    rezultatLabela.innerHTML = "Odabrani spol: " + odabranaVrijednost;
+
+  
+    var odabraniRadioGumb = document.getElementById(odabranaVrijednost.toLowerCase());
+    if (odabraniRadioGumb) {
+        odabraniRadioGumb.checked = true;
+    }
+
+    
+    for (var j = 0; j < lista.options.length; j++) {
+        if (lista.options[j].value === odabranaVrijednost) {
+            lista.selectedIndex = j;
             break;
         }
     }
 }
 
-function postaviIzPadajuceListe(){
+function postaviIzPadajuceListe() {
     var lista = document.getElementById("odabirSpola");
     var odabranaOpcija = lista.options[lista.selectedIndex].value;
 
-    document.getElementById(odabranaOpcija).checked = true;
-    document.getElementById("rezultat").innerHTML = "Odabrani spol" + odabranaOpcija;
+    
+    var odabraniRadioGumb = document.getElementById(odabranaOpcija);
+    if (odabraniRadioGumb) {
+        odabraniRadioGumb.checked = true;
+    }
+
+    document.getElementById("rezultat").innerHTML = "Odabrani spol: " + odabranaOpcija;
 }
 
 function ponisti() {

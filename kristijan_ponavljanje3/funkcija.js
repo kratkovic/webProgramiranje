c027d81b413e871d24ce8aaee994ce261a26e800
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2024-02-14 20:36:51 +0100

    final verzija

--- kristijan_ponavljanje3/funkcija.js
+++ kristijan_ponavljanje3/funkcija.js
@@ -1,3 +1,15 @@
+function postaviIzPadajuceListe() {
+    var lista = document.getElementById("odabirSpola");
+    var odabranaOpcija = lista.options[lista.selectedIndex].value;
+
+    var odabraniRadioGumb = document.getElementById(odabranaOpcija);
+    if (odabraniRadioGumb) {
+        odabraniRadioGumb.checked = true;
+    }
+
+    document.getElementById("rezultat").innerHTML = "Odabrani spol: " + odabranaOpcija;
+}
+
 function postavi() {
     var radioGumbi = document.getElementsByName("spol");
     var lista = document.getElementById("odabirSpola");
@@ -12,43 +24,30 @@ function postavi() {
         }
     }
 
-    rezultatLabela.innerHTML = "Odabrani spol: " + odabranaVrijednost;
-
-  
-    var odabraniRadioGumb = document.getElementById(odabranaVrijednost.toLowerCase());
-    if (odabraniRadioGumb) {
-        odabraniRadioGumb.checked = true;
-    }
-
-    
-    for (var j = 0; j < lista.options.length; j++) {
-        if (lista.options[j].value === odabranaVrijednost) {
-            lista.selectedIndex = j;
-            break;
+    // Postavi odabranu vrijednost u padajuću listu
+    if (odabranaVrijednost) {
+        for (var j = 0; j < lista.options.length; j++) {
+            if (lista.options[j].value === odabranaVrijednost) {
+                lista.selectedIndex = j;
+                break;
+            }
         }
     }
-}
 
-function postaviIzPadajuceListe() {
-    var lista = document.getElementById("odabirSpola");
-    var odabranaOpcija = lista.options[lista.selectedIndex].value;
-
-    
-    var odabraniRadioGumb = document.getElementById(odabranaOpcija);
-    if (odabraniRadioGumb) {
-        odabraniRadioGumb.checked = true;
-    }
-
-    document.getElementById("rezultat").innerHTML = "Odabrani spol: " + odabranaOpcija;
+    // Ažuriraj rezultat na labeli ispod
+    rezultatLabela.innerHTML = "Odabrani spol: " + odabranaVrijednost;
 }
 
 function ponisti() {
     var radioGumbi = document.getElementsByName("spol");
+    var lista = document.getElementById("odabirSpola");
     var rezultatLabela = document.getElementById("rezultat");
 
     for (var i = 0; i < radioGumbi.length; i++) {
         radioGumbi[i].checked = false;
     }
+
+    lista.selectedIndex = 0;
 
     rezultatLabela.innerHTML = "";
 }

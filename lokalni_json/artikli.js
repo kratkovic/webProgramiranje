9957a47a3325bce4b3928382e04d57d4eabe536b
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2024-03-08 22:49:54 +0100

    update artikli sa funkcijom

--- lokalni_json/artikli.js
+++ lokalni_json/artikli.js
@@ -1,26 +1,27 @@
-function osvjeziTablicuArtikla() {
-    var body = document.getElementById("tab_art");
-    //iniciljano postalvjanje prije foreach petlje
-    let bufferZaIspis = "";
-    // foreach element iz polja podaci (napunjen u podaci.js skripti)
-   artikli.forEach(element => {
-        bufferZaIspis += "<tr onclick=\" dohvatiDetaljeArt('" + element.id + "')\" >";
-        bufferZaIspis += "<td></td>";
-        bufferZaIspis += "<td>" + element.brart + "</td>";
-        bufferZaIspis += "<td>" + element.naziv + "</td>";
-        bufferZaIspis += "</tr>";
-    });
-
-
-    //istakanje renedirane vrijedsnoti u body DOM element
-    body.innerHTML = bufferZaIspis;
-
-    document.getElementById("pregled-korisnika").style.display = "none";
-    document.getElementById("izmjena-korisnika").style.display = "none";
-    document.getElementById("pregled-narudžbi").style.display = "none";
-    document.getElementById("pregled-artikala").style.display = "block";
-    document.getElementById("izmjena-narudzbi").style.display = "none";
-    document.getElementById("izmjena-artikla").style.display = "none";
+function osvjeziTablicuArtikala() {
+    fetch('http://localhost:3000/api/artikl')
+        .then(response => response.json())
+        .then(artikli => {
+            var body = document.getElementById("tab_art");
+            //iniciljano postavljanje prije foreach petlje
+            let bufferZaIspis = "";
+            // foreach element iz polja podaci (napunjen u podaci.js skripti)
+            artikli.forEach(element => {
+                bufferZaIspis += "<tr onclick=\"dohvatiDetaljeArt('" + element.id + "')\" >";
+                bufferZaIspis += "<td></td>";
+                bufferZaIspis += "<td>" + element.naziv + "</td>";
+                bufferZaIspis += "<td>" + element.cijena + "</td>";
+                bufferZaIspis += "</tr>";
+            }
+            );
+            //istakanje renderirane vrijednosti u body DOM element
+            body.innerHTML = bufferZaIspis;
+            //aktiviram željeni dio stranice (html dokumenta)
+            aktivirajPregled("pregled-artikala");
+        })
+        .catch(error => {
+            console.error('Greška prilikom dohvata:', error);
+        });
 }
 
 function dohvatiDetaljeArt(x) {
@@ -52,13 +53,13 @@ function spremiIzmjeneArtikla() {
             id: odabraniID,
             brart: document.getElementById("brart").value,
             naziv: document.getElementById("naziv").value,
-           
+
         };
         artikli.push(noviClan);
         odabraniID = "";
         document.getElementById("brart").value = "";
         document.getElementById("naziv").value = "";
-        } else {
+    } else {
 
         //tražim element iz polja koji ima id jednak vrijednosti parametra x
         for (let i = 0; i < artikli.length; i++) {
@@ -66,7 +67,7 @@ function spremiIzmjeneArtikla() {
                 //kada nađemo element moramo napuniti formu
                 artikli[i].brart = document.getElementById("brart").value;
                 artikli[i].naziv = document.getElementById("naziv").value;
-              
+
 
 
                 break;
--- node_server/simpleserver_crud.js
+++ node_server/simpleserver_crud.js
@@ -94,3 +94,6 @@ const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+
